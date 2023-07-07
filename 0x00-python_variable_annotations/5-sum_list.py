9f56befaf92c4c9575a8cd6ad55b8e176a0df204
kokole12 <kokoleismail886@gmail.com>
2023-07-07 10:00:42 +0300

    5-sum_list.py

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -4,7 +4,9 @@ from typing import List
 
 
 def sum_list(input_list: List[float]) -> float:
+    """looping to find the sum"""
     sum: float = 0
     for i in input_list:
         sum += i
+    """returning the sum"""
     return float(sum)
