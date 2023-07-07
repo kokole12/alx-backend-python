a2e6d6ab0196c29972ac0f745c1965b30131e5e8
kokole12 <kokoleismail886@gmail.com>
2023-07-07 10:05:52 +0300

    fixing documentation challenge

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,7 +1,8 @@
-#!/usr/bin/node
+#!/usr/bin/env python3
 """annotating function for the correct output"""
 from typing import Sequence, Iterable, List, Tuple
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """returning the result"""
     return [(i, len(i)) for i in lst]
