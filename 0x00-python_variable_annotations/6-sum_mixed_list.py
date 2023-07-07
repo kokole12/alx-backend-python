9122f5145d46fa11342cd57e234055f341eb9a8a
kokole12 <kokoleismail886@gmail.com>
2023-07-07 10:02:19 +0300

    fixing documentation challenge

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -4,7 +4,9 @@ from typing import List, Union
 
 
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """using for loop to get the sum"""
     sum: float = 0.0
     for i in mxd_lst:
         sum += i
+    """returning the sum with type float"""
     return float(sum)
