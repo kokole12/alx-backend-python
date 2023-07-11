070c86e062d21c4d9044d90859ef5181439fe6d5
kokole12 <kokoleismail886@gmail.com>
2023-07-11 20:11:35 +0300

    coroutines to return 10 random values

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -7,5 +7,5 @@ async_generator = __import__('0-async_generator').async_generator
 
 async def async_comprehension() -> List[float]:
     """returning the random numbers"""
-    l = [i async for i in async_generator()]
-    return l
+    list = [i async for i in async_generator()]
+    return list
