7df4ac75dae7daaf7df27767f6baa2254160d88b
kokole12 <kokoleismail886@gmail.com>
2023-07-11 20:28:55 +0300

    determinging the runnig time of a coroutine

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""measuring the running time of coroutines"""
+import time
+import asyncio
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """Measure the total runtime"""
+    start_time = time.time()
+
+    await asyncio.gather(*(async_comprehension() for i in range(4)))
+
+    end_time = time.time()
+    return end_time - start_time
